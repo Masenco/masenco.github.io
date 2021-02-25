2a3d200126b3ac4071f18e3df6d714ab3ebe892d
Tomás Atampi <tomiatampi@gmail.com>
2021-02-25 17:42:11 -0300

    Commit inicial

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function activarMenu() {
+    const menuToggle = document.querySelector('.toggle-menu');
+    const navegacion = document.querySelector('.nav-moviles');
+    navegacion.classList.toggle('active');
+    menuToggle.classList.toggle('active');
+}
+
+
