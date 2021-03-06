60abcbf8af8f58344d262bc6de829b67bceddee1
Tomás Atampi <tomiatampi@gmail.com>
2021-03-06 18:09:49 -0300

    Actualizacion de navegación móvil

--- scripts.js
+++ scripts.js
@@ -1,8 +1,14 @@
 function activarMenu() {
     const menuToggle = document.querySelector('.toggle-menu');
-    const navegacion = document.querySelector('.nav-moviles');
-    navegacion.classList.toggle('active');
+    const nav = document.querySelector('.nav');
     menuToggle.classList.toggle('active');
+    nav.classList.toggle('active');
 }
 
+function desactivarMenu () {
+    const desactivarMenu = document.querySelector('.nav');
+    desactivarMenu.classList.remove('active');
+    const cerrarToggle = document.querySelector('.toggle-menu');
+    cerrarToggle.classList.remove('active');
+}
 
